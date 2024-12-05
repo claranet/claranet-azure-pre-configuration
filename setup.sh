07d55542ffc88447ce25b4a944cc61d026d072ec
Rémi Beaufils <remi.beaufils@claranet.com>
2024-12-05 11:19:43 +0100

    fix: role assignment issue due to AzCLI parameters change

--- setup.sh
+++ setup.sh
@@ -204,7 +204,7 @@ do
       for try in {1..10}
       do
         # We need to loop due to Azure AD propagation latency
-        az role assignment create --assignee "${SP_HASH[$SP_NAME"_APP_ID"]}" --role "$role" --subscription "$SUBSCRIPTION_ID" > /dev/null 2>&1 && SUCCESS="Yes" && break
+        az role assignment create --assignee "${SP_HASH[$SP_NAME"_APP_ID"]}" --role "$role" --scope "/subscriptions/$SUBSCRIPTION_ID" > /dev/null 2>&1 && SUCCESS="Yes" && break
         echo -n "."
         sleep 3
       done
@@ -227,7 +227,7 @@ do
         for try in {1..30}
         do
           # We need to loop due to Azure AD propagation latency
-          az role assignment create --assignee "${SP_HASH[$SP_NAME_DEPLOY"_APP_ID"]}" --role "$role" --subscription "$SUBSCRIPTION_ID" > /dev/null 2>&1 && break
+          az role assignment create --assignee "${SP_HASH[$SP_NAME_DEPLOY"_APP_ID"]}" --role "$role" --scope "/subscriptions/$SUBSCRIPTION_ID" > /dev/null 2>&1 && break
           echo -n "."
           sleep 3
         done
@@ -299,7 +299,7 @@ then
       for try in {1..30}
       do
         # We need to loop due to Azure AD propagation latency
-        az role assignment create --assignee "$GROUP_OBJECT_ID" --role "$GROUP_ROLE" --subscription "$SUB" > /dev/null 2>&1 && break
+        az role assignment create --assignee "$GROUP_OBJECT_ID" --role "$GROUP_ROLE" --scope "/subscriptions/$SUB" > /dev/null 2>&1 && break
         sleep 3
       done
       echo "Done assigning role"
